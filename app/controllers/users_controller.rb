139fa1a0e1123234d8b0f094dcf19b706b5d98b4
Elmer Tuazon <appearonclouds@gmail.com>
2019-12-22 17:05:20 +0800

    fix register/login and posting

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+    def index
+    end
+
+    def checkCredentials
+        @user = User.where(email: params[:user][:email]).first
+        
+
+        if @user.present?
+            pw = @user.password_digest == params[:user][:password_digest] ? true : false
+            if pw
+                redirect_to user_articles_path(@user.id)
+            else
+                flash[:danger] = 'Incorrect Login Credentials'
+                redirect_to root_path
+            end
+        else
+            flash[:danger] = 'Incorrect Login Credentials'
+            redirect_to root_path
+        end
+
+        # render json: {
+        #     data: @user.present?
+        # }
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        @user.save
+        flash[:success] = 'Successfully Registered'
+        redirect_to root_path
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :email, :password_digest)
+        end
+
+    private
+        def user_email_params
+            params.require(:user).permit(:email, :email)
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get 'login', to: 'users#login'
+  post 'loginCheck', to: 'users#checkCredentials'
+
+  resources :users do
+    resources :articles
+  end
+
   resources :articles do
     resources :comments
   end
-  
+
   root 'welcome#index'
 end
